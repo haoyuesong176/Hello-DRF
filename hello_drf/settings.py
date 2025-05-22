11caf7fd67a16df3ff129159ba33b4a60875f32c
Haoyue <407735995@qq.com>
2025-05-22 23:39:38 +0800

    snapshot

--- hello_drf/settings.py
+++ hello_drf/settings.py
@@ -163,8 +163,8 @@ REST_FRAMEWORK = {
 }
 
 SIMPLE_JWT = {
-    'ACCESS_TOKEN_LIFETIME': timedelta(seconds=5),
-    'REFRESH_TOKEN_LIFETIME': timedelta(seconds=10),
+    'ACCESS_TOKEN_LIFETIME': timedelta(minutes=15),
+    'REFRESH_TOKEN_LIFETIME': timedelta(days=7),
     'ROTATE_REFRESH_TOKENS': True, 
 }
 
